b23b7bf2979670dd864a027f92319dbced78c827
Ed Maphis <emaphis85@gmail.com>
2016-04-08 17:44:27 -0400

    Get basebull function working

--- basebull/src/Main.hs
+++ basebull/src/Main.hs
@@ -2,4 +2,25 @@
 
 module Main where
 
-main = putStrLn "hello"
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+-- from cassava
+import Data.Csv
+
+-- a simple type alias for data
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
+
+fourth :: (a, b, c, d) -> d
+fourth (_, _, _, d) = d
+
+baseballStats ::BL.ByteString -> Either String (V.Vector BaseballStats)
+baseballStats  = decode NoHeader
+
+
+main :: IO ()
+main = do
+  csvData <- BL.readFile "batting.csv"
+  let summed = fmap (V.foldr summer 0) (baseballStats csvData)
+  putStrLn $ "Total atBats was: " ++ show summed
+  where summer = (+). fourth
