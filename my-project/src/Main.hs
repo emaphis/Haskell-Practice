6e18a1f71a6ff083a3ce4ed2851248e68b72cd56
Ed Maphis <emaphis85@gmail.com>
2016-04-09 19:52:27 -0400

    Using Monads - Input Actions (Monads)

--- my-project/src/Main.hs
+++ my-project/src/Main.hs
@@ -3,9 +3,23 @@ module Main where
 action :: IO ()
 action = putStrLn "Hello World"
 
+sayHello :: IO String
+sayHello = do
+  name <- getLine
+  putStrLn ("Hello " ++ name)
+  return name
+
+beCareful :: Maybe Int
+beCareful = do
+  Just 6
+--  putStrLn "oops"
+  Nothing
+  return 5
+
 main :: IO ()
 main = do
-  action
-  action
+  let action1 = putStrLn "Hello World"
+  action1
+  action1
   action
   return ()
