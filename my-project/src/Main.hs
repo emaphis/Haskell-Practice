91c330cb71d50583d143befa4f374b97e4ed9e2c
Ed Maphis <emaphis85@gmail.com>
2016-04-09 19:22:54 -0400

    Using Monads - Output actions (monads)

--- my-project/src/Main.hs
+++ my-project/src/Main.hs
@@ -1,26 +1,11 @@
 module Main where
 
---import System.IO (readFile)
-import Data.Time (getCurrentTime)
-
-greet :: String -> String
-greet name = "Hello " ++ name ++ "!"
-
-printNumbers = do
-  putStrLn (show (3+4))
-
-printConfig = do
-  contents <- readFile "stack.yaml"
-  putStrLn contents
-
-printTime = do
-  time <- getCurrentTime
-  putStrLn (show time)
+action :: IO ()
+action = putStrLn "Hello World"
 
 main :: IO ()
 main = do
-  putStrLn (greet "bobby")
-  putStrLn (greet "World")
-  printNumbers
-  printConfig
-  printTime
+  action
+  action
+  action
+  return ()
