ca797aee5a2266895d1a00a9e1ee912492df608d
Ed Maphis <emaphis85@gmail.com>
2016-04-12 12:20:47 -0400

    CIS 194 Week one work

---
+++ cis194/src/Lyah02.hs
@@ -0,0 +1,29 @@
+-- | LYAH Chapter 2 - Starting Out
+-- <http://learnyouahaskell.com/starting-out>
+
+module Lyah02 where
+
+doubleMe :: Num a => a -> a
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+-- double numbers smaller than 100
+doubleSmallNumber x = if x > 100
+                      then x
+                      else x*2
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+lc2 = [x*y | x <-  [2,5,10], y <- [8,10,11]]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase str = [c | c <- str, c `elem` ['A'..'Z']]
+
+-- triangles
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24] 
