203de65653fe14786556d8edf5329780f9e67dd0
Ed Maphis <emaphis85@gmail.com>
2016-04-19 16:14:21 -0400

    Make the Hanoi design more explicit.

--- cis194/src/Week01/Hanoi.hs
+++ cis194/src/Week01/Hanoi.hs
@@ -8,5 +8,8 @@ type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _  = []  -- base case
-hanoi n t1 t2 t3 =
-  hanoi (n-1) t1 t3 t2 ++ [(t1, t2)] ++ hanoi (n-1) t3 t2 t1
+hanoi n t1 t2 t3 = step1 ++ [step2] ++ step3
+  where
+    step1 = hanoi (n-1) t1 t3 t2
+    step2 = (t1, t2)
+    step3 = hanoi (n-1) t3 t2 t1
